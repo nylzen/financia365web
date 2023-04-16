639cf6b85f85ab4e95309cbdb269ade9f1822bf8
Nylzen <tugoresn@gmail.com>
2023-04-16 16:39:44 -0300

    add: Title Home

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,10 +8,16 @@ import {
   Testimonials,
 } from '@/components';
 import { Box } from '@chakra-ui/react';
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Financia365</title>
+        <meta name='description' content='Financia365' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      </Head>
       <Box bg='gray.100'>
         <Hero />
       </Box>
